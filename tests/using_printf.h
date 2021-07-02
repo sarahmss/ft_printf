a57f1db3cd31995f43d8bcfac84eea3a985965f9
sarahmss <sarahmodestos@gmail.com>
2021-07-02 10:18:50 -0300

    +tests

---
+++ tests/using_printf.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   using_printf.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smodesto <smodesto@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/17 11:08:41 by smodesto          #+#    #+#             */
+/*   Updated: 2021/06/17 11:14:48 by smodesto         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef USING_PRINTF_H
+#define USING_PRINTF_H
+
+#define ANSI_RED "\033[1;31m"
+
+#define ANSI_YELLOW "\e[0;93m"
+
+#define ANSI_CYAN "\e[0;36m"
+
+#define ANSI_PURPLE "\e[0;35m"
+
+#define ANSI_BLUE "\e[0;34m"
+
+#define ANSI_GREEN "\e[0;32m"
+
+#define ANSI_RESET "\033[0m"
+
+
+#endif
