70a84fa3d6b38f7450a7ced449b961345b8134d2
sarahmss <sarahmodestos@gmail.com>
2021-07-08 08:15:23 -0300

    c

--- ft_flags.c
+++ ft_flags.c
@@ -6,7 +6,7 @@
 /*   By: smodesto <smodesto@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/28 16:50:49 by smodesto          #+#    #+#             */
-/*   Updated: 2021/07/07 19:59:21 by smodesto         ###   ########.fr       */
+/*   Updated: 2021/07/08 08:15:00 by smodesto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	ft_flags(t_format *tab)
 	int	print;
 
 	prec = tab->precision;
-	if ((tab->width == 1 || tab->width_prm[0] != 0) && (tab->pad_zero == 0))
+	if ((tab->width == 1 || tab->width_prm[1] != 0) && (tab->pad_zero == 0))
 		ft_width(tab);
 	if (tab->pad_zero == 1 && tab->l_just == 0)
 		ft_padzero(tab);
