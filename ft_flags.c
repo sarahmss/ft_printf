9b509b668e6d15855fb70eb681ae8f4f74145d27
sarahmss <sarahmodestos@gmail.com>
2021-07-08 09:00:04 -0300

    testing prm + pointer broken

--- ft_flags.c
+++ ft_flags.c
@@ -6,7 +6,7 @@
 /*   By: smodesto <smodesto@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/28 16:50:49 by smodesto          #+#    #+#             */
-/*   Updated: 2021/07/08 08:50:28 by smodesto         ###   ########.fr       */
+/*   Updated: 2021/07/08 08:59:38 by smodesto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,11 +33,7 @@ static int	ft_sup(t_format *tab, char *stemp, int i)
 	char	width[10];
 
 	if (tab->width_prm[0] != 0)
-	{
 		i = tab->width_prm[0];
-		if (i < 0)
-			i *= -1;
-	}
 	else
 	{
 		while (ft_isdigit(*stemp) || *stemp == '-')
@@ -98,6 +94,10 @@ int	ft_prm(t_format *tab, char *stemp)
 	else
 		tab->width_prm[0] = va_arg(tab->args, int);
 	if (tab->width_prm[0] < 0)
+	{
 		tab->l_just = 1;
+		tab->width_prm[0] *= -1;
+	}
+
 	return (0);
 }
