52edf2c2d3d7f303f1a9adf7911756be259c5467
sarahmss <sarahmodestos@gmail.com>
2021-07-08 11:07:11 -0300

    s16?

--- ft_precision.c
+++ ft_precision.c
@@ -6,34 +6,46 @@
 /*   By: smodesto <smodesto@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/02 09:24:29 by smodesto          #+#    #+#             */
-/*   Updated: 2021/07/08 10:00:10 by smodesto         ###   ########.fr       */
+/*   Updated: 2021/07/08 11:04:39 by smodesto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/ft_printf.h"
 
+static int	ft_sup(char *stemp)
+{
+	char	width[10];
+	int		i;
+
+	i = 0;
+	while (ft_isdigit(*stemp) || *stemp == '.' || *stemp == '-')
+	{
+		if (ft_isdigit(*stemp))
+			width[i++] = *stemp;
+		stemp++;
+	}
+	stemp = width;
+	i = ft_atoi(stemp);
+	if (i == 0)
+		i = -1;
+	return (i);
+}
+
 int	ft_precision(t_format *tab)
 {
 	int		i;
-	char	width[10];
 	char	*stemp;
 
 	stemp = tab->temp;
-	i = 0;
+	i = -1;
+	while (*stemp != '.' )
+		stemp++;
 	stemp++;
 	if (*stemp == '*')
 		i = va_arg(tab->args, int);
+	else if (!ft_isdigit(*stemp))
+		return (i);
 	else
-	{
-		while (ft_isdigit(*stemp) || *stemp == '.' || *stemp == '-')
-		{
-			if (ft_isdigit(*stemp) || *stemp == '.' )
-				width[i++] = *stemp;
-			stemp++;
-		}
-		stemp = ft_strchr(width, '.');
-		stemp++;
-		i = ft_atoi(stemp);
-	}
+		i = ft_sup(stemp);
 	return (i);
 }
